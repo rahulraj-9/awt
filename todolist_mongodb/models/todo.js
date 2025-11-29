316a59955e4027a60f61a6456f8b4653b0e91f20
unknown <r.rahul74618@gmail.com>
2025-11-29 10:50:47 +0530

    add to do app

--- todolist_mongodb/models/todo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const todoSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  text: String,
-  done: Boolean
-});
-
-module.exports = mongoose.model('Todo', todoSchema);
